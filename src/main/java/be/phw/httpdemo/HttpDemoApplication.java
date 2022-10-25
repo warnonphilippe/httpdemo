30eb6ebb6aa6e60b5055ed4ba08bf9e57e14779f
philippe <phw@civadis.be>
2022-10-25 22:01:28 +0200

    initial commit

---
+++ src/main/java/be/phw/httpdemo/HttpDemoApplication.java
@@ -0,0 +1,43 @@
+package be.phw.httpdemo;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+ 
+@SpringBootApplication
+public class HttpDemoApplication {
+
+	// port to listen connection
+	static final int PORT = 8081;
+
+	// verbose mode
+	static final boolean verbose = true;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpDemoApplication.class, args);
+		httpServer(args);
+	}
+
+	public static void httpServer(String[] args) {
+		try {
+			ServerSocket serverConnect = new ServerSocket(PORT);
+			System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+			ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+			
+			// we listen until user halts server execution
+			while (true) {
+				executor.execute(new JavaHTTPServer(serverConnect.accept()));
+			}
+			
+		} catch (IOException e) {
+			System.err.println("Server Connection error : " + e.getMessage());
+		}
+	}
+
+}
